6fd34c8e91e42091d9f44d84f4e7f33853bfa51c
Javier Solis <111769866+javiepic@users.noreply.github.com>
2022-10-03 15:32:15 +0000

    fixing

--- .ccls-cache/@home@runner@AstrologicalStars/main.cpp
+++ .ccls-cache/@home@runner@AstrologicalStars/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 // Functions 
-void Astro(int, int );
+int Astro(string, int);
 
 int main() {
   // values 
@@ -13,57 +13,30 @@ int main() {
   cin >> d;
   cin >> m;
   //
-  if (m == 'Jan')
-    i_0 = 1;
-  else if (m == 'Feb')
-    i_0 = 2;
-  else if (m == 'Mar')
-    i_0 = 3;
-  else if (m == 'Apr')
-    i_0 = 4;
-  else if (m == 'May')
-    i_0 = 5;
-  else if (m == 'Jun')
-    i_0 = 6;
-   else if (m == 'Jul')
-    i_0 = 7;
-      else if (m == 'Aug')
-    i_0 = 8;
-  else if (m == 'Sep')
-    i_0 = 9;
-   else if (m == 'Oct')
-    i_0 = 10;
-      else if (m == 'Nov')
-    i_0 = 11;
-   else if (m == 'Dec')
-    i_0 = 12;
+  Astro(m, i_0);
   
-  
-}
-void Astro(int i_o, int d)
-(
-switch (i_0)
+  switch (i_0)
 {
 case 1:
-  if ((i_0 =< 1 ) || (i_0 >=20))
+  if (i_0 >=20)
   cout << "Your Astrological sign is Capricorn."<<endl;
 else 
   cout << "Your Astrological sign is Aquarius."<< endl;
 break;
 case 2:
-  if ((i_0 =< 1 ) || (i_0 >=19))
+  if (i_0 >=19)
   cout << "Your Astrological sign is Aquarius."<<endl;
 else 
   cout << "Your Astrological sign is Pisces."<< endl;
 break;
 case 3:
-  if ((i_0 =< 1 ) || (i_0 >=20))
+  if (i_0 >=20)
   cout << "Your Astrological sign is Pisces."<<endl;
 else 
   cout << "Your Astrological sign is Aries."<< endl;
 break;
 case 4:
-  if ((i_0 =< 1 ) || (i_0 >=20))
+  if (i_0 >=20)
   cout << "Your Astrological sign is Aries."<<endl;
 else 
   cout << "Your Astrological sign is Taurus."<< endl;
@@ -117,5 +90,36 @@ else
   cout << "Your Astrological sign is Capricon."<< endl;
 break;
   }
-return 0;
+  return 0;
+  
+}
+int Astro(string m)
+(
+
+  if (m == 'Jan')
+    return 1;
+  else if (m == 'Feb')
+    return 2;
+  else if (m == 'Mar')
+   return 3;
+  else if (m == 'Apr')
+   return 4;
+  else if (m == 'May')
+    return 5;
+  else if (m == 'Jun')
+    return 6;
+   else if (m == 'Jul')
+    return 7;
+      else if (m == 'Aug')
+    return 8;
+  else if (m == 'Sep')
+    return 9;
+   else if (m == 'Oct')
+    return 10;
+      else if (m == 'Nov')
+    return 11;
+   else if (m == 'Dec')
+    return 12;
+else
+return -1;
 );
